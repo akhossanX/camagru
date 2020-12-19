ec9350a9773454466238bbbe9aa6d54a449af62f
akhossanX <abdelilah.khossan@gmail.com>
2020-12-19 14:54:05 +0100

    stickers mouse drag and drop in progress

--- app/views/users/camera.php
+++ app/views/users/camera.php
@@ -1,6 +1,10 @@
 <?php require_once APPROOT . '/views/inc/header.php'; ?>
 <?php require_once APPROOT . '/views/inc/navbar.php'; ?>
-    <h1>Camera Page</h1>
+    <div class="container">
+        <div class="jumbotron">
+            <p>Select stickers and shot your picture !</p>
+        </div>
+    </div>
     <?php
         $stickers = null;
         $ret = null;
@@ -8,19 +12,19 @@
     ?>
     <div class="stickers" id="stickers">
         <?php foreach ($stickers as $png): ?>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" value="" id="{$png}"/>
-                <label class="form-check-label" for="{$png}"> <img src='<?=URLROOT . "/public/img/${png}"?>'></label>
-            </div>
+            <img src='<?=URLROOT . "/public/img/${png}"?>'>
         <?endforeach;?>
     </div>
+
     <br><br>
-    <div class="video-container">
-        <video id="video"></video>
-        <!-- <img id="sticker" src=""> -->
+
+    <div class="video-container" id="video-container-id">
+        <video id="video-id"></video>
     </div>
+
     <div>
         <canvas id="canvas"></canvas>
     </div>
+
     <script src="<?=URLROOT?>/public/js/camera.js"></script>
 <?php require_once APPROOT . '/views/inc/footer.php'; ?>
--- public/js/camera.js
+++ public/js/camera.js
@@ -1,15 +1,12 @@
 'use strict';
 
-var video = $('#video')
-
-
 function $(selector) {
     return document.querySelector(selector)
 }
 
 function stream_init() {
     if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({video: true})
+        navigator.mediaDevices.getUserMedia(constraints)
         .then(function (stream) {
             video.srcObject = stream
             video.play()
@@ -21,37 +18,81 @@ function stream_init() {
     }
 }
 
-stream_init()
-
-// img.src = 'https://assets.stickpng.com/images/580b585b2edbce24c47b2a1f.png'
-// img.parentElement.style.position = 'relative';
-// img.style.position = 'absolute';
-// img.style.left = '0px';
-// img.style.top = '0px';
-// console.log(img.parentElement);
-// console.log("parent: " + img.parentElement.offsetLeft + ", " + img.parentElement.offsetTop)
-// console.log("img: " + img.offsetLeft + ", " + img.offsetTop)
-
-var checkboxes = document.querySelectorAll('#stickers .form-check .form-check-input'),
-    pngs = document.querySelectorAll('#stickers .form-check .form-check-label img')
-    console.log(pngs)
-    console.log(checkboxes)
-var topCorner = 0, leftCorner = 0
-
 function onStickerClick() {
-    if (this.checked === true) {
-        var img = document.createElement('img')
-        img.style.position = 'absolute';
-        img.style.left = leftCorner + 'px'
-        img.style.top = topCorner + 'px'
-        img.src = pngs[0].src
-        leftCorner += 100
-        topCorner += 100
-        video.insertAdjacent('afterend', img.toString())
+    var img = document.createElement('img')
+    img.style.position = 'absolute';
+    img.style.left = leftCorner + 'px'
+    img.style.top = topCorner + 'px'
+    img.style.width = '100px';
+    img.style.height = '100px';
+    img.src = this.src
+    img.ondblclick = OnStickerDoubleClick
+    img.onmousedown = onStickerMouseDown
+    img.onmousemove = onStickerMouseMove
+    img.onmousemove = onStickerMouseUp
+    video.insertAdjacentElement('afterEnd', img)
+}
+
+function OnStickerDoubleClick() {
+    this.remove()
+}
+
+function onStickerMouseUp() {
+    if (isdown === true) {
+        console.log('Mouse Up\n')
+        isdown = false
     }
 }
-var i
-for (i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].onclick = onStickerClick
+
+function onStickerMouseDown() {
+    // console.log(this.offsetLeft)
+    // console.log(this.offsetTop)
+    isdown = true
+
+    console.log('Mouse Down\n')
 }
+
+function onStickerMouseMove() {
+    event.preventDefault();
+    if (isdown === true) {
+        console.log("Mouse Move \n")
+        console.log(event.clientX)
+        console.log(event.clientY)
+        this.style.left += event.clientX - parseInt(this.style.left) + 'px' 
+        this.style.top += event.clientY - parseInt(this.style.top) + 'px'
+        console.log(this.style.left)
+        console.log(this.style.top)
+    }
+}
+
+
+var video = $('#video-id'),
+    videoContainer = document.getElementById('video-container-id')
+
+
+var styles = window.getComputedStyle(videoContainer, null)
+var width = parseInt(styles.getPropertyValue('width'))
+var height = parseInt(styles.getPropertyValue('height'))
+
+// console.log(width)
+// console.log(height)
+
+var constraints = {
+    video: {
+        width: width - 8,
+        height: height - 8
+    }
+}
+
+stream_init()
+
+
+var stickers = document.querySelectorAll('#stickers img')
+
+var topCorner = 100, leftCorner = 100
+
+stickers.forEach(sticker => sticker.onclick = onStickerClick)
+
+var isdown = false
+
     
