8aa8c927fc185bfa0d9ffaeaf8e63fc13f51cc50
akhossanX <abdelilah.khossan@gmail.com>
2020-12-23 19:07:19 +0100

    positionning the sticker on move in progress

--- public/js/camera.js
+++ public/js/camera.js
@@ -27,9 +27,9 @@ function onStickerClick() {
     img.style.height = '100px';
     img.src = this.src
     img.ondblclick = OnStickerDoubleClick
-    img.onmousedown = onStickerMouseDown
-    img.onmousemove = onStickerMouseMove
-    img.onmousemove = onStickerMouseUp
+    img.draggable = true
+    img.id = '_' + Math.random().toString(36)
+    img.ondragstart = onStickerDragStart
     video.insertAdjacentElement('afterEnd', img)
 }
 
@@ -37,45 +37,42 @@ function OnStickerDoubleClick() {
     this.remove()
 }
 
-function onStickerMouseUp() {
-    if (isdown === true) {
-        console.log('Mouse Up\n')
-        isdown = false
+function onStickerDragStart(event) {
+    console.log('Drag Start: ' + event.type)
+    console.log(this.id)
+    event.dataTransfer.setData('text/plain', this.id)
+} 
+
+function onStickerDragOver(event) {
+    event.preventDefault()
+    event.dataTransfer.dropEffect = 'move'
+}
+
+function onStickerDrop(event) {
+    console.log('Drop Event: ' + event.type)
+    var liveSticker = document.getElementById(event.dataTransfer.getData('text/plain'))
+    console.log('video position: (' + videoPosition.left + ', ' + videoPosition.top + ')')
+    console.log('mouse absolute position: (' + event.clientX + ', ' + event.clientY + ')')
+    var translationVector = {
+        x: event.clientX - videoPosition.left,
+        y: event.clientY - videoPosition.top
     }
+    console.log(translationVector)
+    console.log('mouse relatvie position: ' + parseInt(event.clientX + translationVector.x) + ', '
+        + parseInt(event.clientY + translationVector.y))
+    console.log('element new position: ' + 
+                liveSticker.style.left + translationVector.x + ', ' + 
+                liveSticker.style.top + translationVector.y)
+    liveSticker.style.left = parseInt(liveSticker.style.left) +  translationVector.x + 'px'
+    liveSticker.style.top = parseInt(liveSticker.style.top) + translationVector.y + 'px'
 }
-
-function onStickerMouseDown() {
-    // console.log(this.offsetLeft)
-    // console.log(this.offsetTop)
-    isdown = true
-
-    console.log('Mouse Down\n')
-}
-
-function onStickerMouseMove() {
-    event.preventDefault();
-    if (isdown === true) {
-        console.log("Mouse Move \n")
-        console.log(event.clientX)
-        console.log(event.clientY)
-        this.style.left += event.clientX - parseInt(this.style.left) + 'px' 
-        this.style.top += event.clientY - parseInt(this.style.top) + 'px'
-        console.log(this.style.left)
-        console.log(this.style.top)
-    }
-}
-
 
 var video = $('#video-id'),
     videoContainer = document.getElementById('video-container-id')
 
-
 var styles = window.getComputedStyle(videoContainer, null)
 var width = parseInt(styles.getPropertyValue('width'))
 var height = parseInt(styles.getPropertyValue('height'))
-
-// console.log(width)
-// console.log(height)
 
 var constraints = {
     video: {
@@ -86,13 +83,22 @@ var constraints = {
 
 stream_init()
 
-
 var stickers = document.querySelectorAll('#stickers img')
 
 var topCorner = 100, leftCorner = 100
 
 stickers.forEach(sticker => sticker.onclick = onStickerClick)
 
-var isdown = false
+video.ondrop = onStickerDrop
+video.ondragover = onStickerDragOver
+
+var videRect = video.getBoundingClientRect()
+var videoPosition = {
+    left: videRect.left + document.documentElement.scrollLeft,
+    top: videRect.top + document.documentElement.scrollTop
+}
+
+console.log('vide pos: ' + videoPosition.left + ', ', + videoPosition.top)
+
 
     
