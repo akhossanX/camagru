cff0d78ace18dfe2e66ea6eb37ce652c571e2efc
akhossanX <abdelilah.khossan@gmail.com>
2021-02-24 10:19:20 +0100

    removed landing page old layout

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -7,34 +7,6 @@
 	<link rel="stylesheet" href="<?=URLROOT;?>/public/css/landing.css">
 </head>
 <body>
-	<header class="v-header container">
-		<div class="video-wrapper">
-			<video class="video" autoplay loop muted>
-				<source src="<?=URLROOT.'/public/intro.mp4'?>" type="video/mp4">
-			</video>
-		</div>
-		<div class="header-overlay"></div>
-		<div class="header-content">
-			<h1>WELCOME TO CAMAGRU</h1>
-			<p>TAKE pictures and ENJOY adding awsome stickers on top of them</p>
-		</div>
-	</header>
-  <section class="section section-a">
-    <div class="container">
-      <h2>What's next</h2>
-      <p></p>
-	</div>
-	<div class="buttons">
-		<a href="<?=URLROOT?>/users/register" class="btn btn-danger btn-lg">
-			REGISTER
-		</a>
-		<a href="<?=URLROOT?>/home/gallery" class="btn btn-danger btn-lg">
-			PUBLIC GALLERY
-		</a>
-		<a href="<?=URLROOT?>/users/login" class="btn btn-danger btn-lg">
-			LOGIN
-		</a>
-	</div>
-  </section>
+
 </body>
 </html>
