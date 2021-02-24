b0086b567988449b98b54e284634fd04d953337b
akhossanX <abdelilah.khossan@gmail.com>
2021-02-24 10:11:27 +0000

    added user avatar to post

--- app/views/home/gallery.php
+++ app/views/home/gallery.php
@@ -1,10 +1,10 @@
 <?php require_once APPROOT . '/views/inc/header.php'; ?>
 <?php require_once APPROOT . '/views/inc/navbar.php'; ?>
     <section class="img-post">
-        <div class="usr-pseudo"><?$_SESSION['images'][0]->username?></div>
+        <div class="user-pseudo"><?= $_SESSION['images'][0]->username ?></div>
         <div class="ca-border" style="width:100%"></div>
         <div class="post">
-            <img src="data:image/png;base64, <?= $_SESSION['images'][0]->data; ?>" alt="">
+            <img src="data:image/png;base64, <?= $_SESSION['images'][0]->data ?>" alt="">
         </div>
         <section class="post-infos">
             <img src="<?= URLROOT . '/public/icons/like.svg'; ?>" style="width: 1.7em; height:1.7em;" />
